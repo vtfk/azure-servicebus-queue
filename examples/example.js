70b1eadef19af630325bd24ed24f7bce64fcfe5b
Jonas Enge <jonas.enge@t-fk.no>
2019-04-09 23:39:27 +0200

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -3,14 +3,14 @@
   const log = data => console.log(JSON.stringify(data, null, 2))
   const connectionString = process.env.QUEUE_CONNECTION_STRING
   const queueName = process.env.QUEUE_NAME
-  const namespace = require('../src/index')({
+  const serviceBusClient = require('../src/index')({
     connectionString
   })
-  const queue = namespace.queue(queueName)
+  const queue = serviceBusClient.queue(queueName)
   try {
     const messages = await queue.peek()
     log(messages)
   } finally {
-    await namespace.close()
+    await serviceBusClient.close()
   }
 })()
